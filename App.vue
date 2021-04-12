acbbd6f0d483a98fbf829e1a117db46840b69f95
David Sodré Lins <david.silva@acpgroup.com.br>
2021-04-11 22:38:46 -0300

    VueNative

---
+++ App.vue
@@ -0,0 +1,98 @@
+<template>
+  <view class="container">
+    <text v-if="!permitido" class="bem_vindo"> {{ nome }}</text>
+    <text-input v-if="!permitido" class="txt_input" v-model="login" />
+
+    <text-input
+      v-if="!permitido"
+      class="txt_input"
+      type="password"
+      v-model="senha"
+    />
+
+    <text v-if="!permitido" class="txt_mensagem"> {{ mensagem }}</text>
+
+    <button
+      v-if="!permitido"
+      class="botao_principal"
+      :title="titulo"
+      v-bind:on-press="acessar"
+    />
+
+    <Principal v-if="permitido" :login="login" />
+  </view>
+</template>
+<script>
+import Principal from "./principal";
+
+export default {
+  components: { Principal },
+
+  data() {
+    return {
+      usuarios: [
+        { nome: "luã", senha: "l123" },
+        { nome: "david", senha: "d999" },
+        { nome: "joulbert", senha: "jb23" },
+        { nome: "carina", senha: "c512" },
+        { nome: "amanda", senha: "a045" },
+        { nome: "gabriel", senha: "p483" },
+      ],
+      nome: "Login",
+      titulo: "Acessar",
+      login: null,
+      senha: null,
+      mensagem: null,
+      permitido: false,
+    };
+  },
+
+  methods: {
+    acessar: function () {
+      if (this.login === "rodrigo" && this.senha === "123456") {
+        this.permitido = true;
+      } else {
+        this.mensagem = "Erro na autenticação";
+        this.login = null;
+        this.senha = null;
+      }
+    },
+  },
+};
+</script>
+
+<style>
+.txt_input {
+  width: 90%;
+  border-color: gray;
+  border-width: 1;
+  padding: 10px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+
+.bem_vindo {
+  color: blue;
+}
+
+.botao_principal {
+  margin-top: 20px;
+  color: rgb(53, 76, 109);
+  border-radius: 15px;
+}
+
+.container {
+  background-color: rgb(21, 241, 21);
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+
+.text-color-primary {
+  color: blue;
+}
+
+.txt_mensagem {
+  color: red;
+}
+</style>
---
+++ principal.vue
@@ -0,0 +1,52 @@
+<template>
+  <view class="quadro">
+    <text class="mensagem"> Bem vindo ao sistema</text>
+    <text class="usuario"> {{ login }} </text>
+  </view>
+</template>
+
+<script>
+export default {
+  props: {
+    login: {
+      Type: String,
+      default: "",
+    },
+  },
+
+  methods: {
+    voltar: function () {
+      this.permitido = false;
+    },
+  },
+};
+</script>
+
+<style>
+.mensagem {
+  color: red;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 20px;
+  font-weight: 300;
+}
+.usuario {
+  color: rgb(0, 47, 255);
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 30px;
+}
+
+.botao_principal {
+  margin-top: 20px;
+  color: rgb(53, 76, 109);
+  border-radius: 15px;
+}
+.container {
+  background-color: rgb(21, 241, 21);
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+.text-color-primary {
+  color: blue;
+}
+</style>
